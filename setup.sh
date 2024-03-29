9c5e71b8cefab4d03454abad0d0132d82b0222b0
Alex Loss <alexnssy@gmail.com>
2024-03-29 17:28:20 +0000

    New Additions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+currentDir=$(pwd)
 sed -i s/%%user%%/$USER/g ./main/torrentvpn.service
-sed -i s@%%filepath%%@/home/$USER/torrentvpn/main/torrentvpn.sh@g ./main/torrentvpn.service
+sed -i s@%%filepath%%@$currentDir/main/torrentvpn.sh@g ./main/torrentvpn.service
 sudo mv ./main/torrentvpn.service /etc/systemd/system/
 chmod +x ./main/torrentvpn.sh
 systemctl enable torrentvpn.service
