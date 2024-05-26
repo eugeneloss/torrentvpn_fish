ef11168999d15d44d7f6eccdb7be56d6fadcee0b
AlexNessy <alexnessy@icloud.com>
2024-05-26 11:29:08 +0100

    New Changes

--- main/torrentvpn.sh
+++ main/torrentvpn.sh
@@ -3,10 +3,7 @@ nmcli con show --active | grep -Ei 'wireguard|vpn|tun'
 vpnCode="${PIPESTATUS[1]}"
 pgrep -i 'torrent'
 torrentCode=$?
-if [[ $vpnCode == 1 ]] && [[ $torrentCode == 0 ]]; then
-  pkill -i 'torrent'
-  echo 'torrent killed'
-else
-  echo ''
-fi
+[[ $vpnCode == 1  && $torrentCode == 0 ]] && pkill -i 'torrent' && echo 'torrent killed'
+  
+  
 
--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-currentDir=$(pwd)
 sed -i s/%%user%%/"$USER"/g ./main/torrentvpn.service
-sed -i s@%%filepath%%@"$currentDir"/main/torrentvpn.sh@g ./main/torrentvpn.service
-sudo mv ./main/torrentvpn.service /etc/systemd/system/
+sed -i s@%%filepath%%@"$PWD"/main/torrentvpn.sh@g ./main/torrentvpn.service
+sudo cp ./main/torrentvpn.service /etc/systemd/system/
 chmod +x ./main/torrentvpn.sh
 sudo systemctl enable torrentvpn.service
 sudo systemctl start torrentvpn.service
