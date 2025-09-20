5ea573236b873102b3de41612658dd4baa59848a
Eugene Loss <eugeneloss@pm.me>
2025-09-20 13:13:26 +0100

    new changes sora

--- setup.fish
+++ setup.fish
@@ -1,26 +1,23 @@
 #!/usr/bin/env fish
-# Prepare and install the systemd unit to run the fish script.
-
+# Prepare and install the systemd *user* unit to run the fish script.
 
 set -l svc ./main/torrentvpn.service
 set -l script ./main/torrentvpn.fish
 
-
-# Substitute placeholders in the service file
-sed -i "s/%%user%%/$USER/g" $svc
+# Substitute filepath placeholder only
 sed -i "s@%%filepath%%@$PWD/main/torrentvpn.fish@g" $svc
-
 
 # Ensure the script is executable
 chmod +x $script
 
+# Install systemd service (user)
+mkdir -p ~/.config/systemd/user
+cp $svc ~/.config/systemd/user/
 
-# Install systemd service
-sudo cp $svc /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable torrentvpn.service
-sudo systemctl start torrentvpn.service
-
+systemctl --user daemon-reload
+systemctl --user enable torrentvpn.service
+systemctl --user start torrentvpn.service
 
 # Show current status (non-paging)
-sudo systemctl status --no-pager torrentvpn.service
+systemctl --user status --no-pager torrentvpn.service
+
