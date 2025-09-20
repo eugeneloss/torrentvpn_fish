fe1a3c238740d67a4317af9dd310820579928354
eugeneness <157149616+eugeneloss@users.noreply.github.com>
2025-09-20 12:34:31 +0100

    Update and rename torrentvpn.sh to torrentvpn.fish

---
+++ main/torrentvpn.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+# Purpose: Kill torrent apps if no VPN/WireGuard/TUN connection is active.
+# Runs under systemd with Restart=always; keep it short and idempotent.
+
+
+# If no active VPN connection is detected...
+if not nmcli con show --active | grep -Eiq 'wireguard|vpn|tun'
+# ...and any torrent process is running, kill it.
+if pgrep -if 'torrent' >/dev/null
+pkill -if 'torrent'
+echo (date --iso-8601=seconds)" torrent killed (no VPN)"
+end
+end
--- main/torrentvpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-nmcli con show --active | grep -Ei 'wireguard|vpn|tun'
-vpnCode="${PIPESTATUS[1]}"
-pgrep -i 'torrent'
-torrentCode=$?
-[[ $vpnCode == 1  && $torrentCode == 0 ]] && pkill -i 'torrent' && echo 'torrent killed'
-  
-  
-
