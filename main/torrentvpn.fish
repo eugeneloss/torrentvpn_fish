9c27d8fc6126e13d34a3a286218b43566e6e3f64
eugeneness <157149616+eugeneloss@users.noreply.github.com>
2025-09-20 13:23:12 +0100

    Update torrentvpn.fish

--- main/torrentvpn.fish
+++ main/torrentvpn.fish
@@ -2,11 +2,10 @@
 # Purpose: Kill torrent apps if no VPN/WireGuard/TUN connection is active.
 
 # If no active VPN connection is detected...
-if not /usr/bin/nmcli --terse --color no con show --active | grep -Eiq 'wireguard|vpn|tun'
+if not /usr/bin/nmcli -t -f TYPE con show --active | grep -Eiq 'wireguard|vpn|tun'
     # ...and any torrent process is running, kill it.
     if /usr/bin/pgrep -if 'transmission|qbittorrent|deluge' >/dev/null
         /usr/bin/pkill -if 'transmission|qbittorrent|deluge'
         /usr/bin/logger -t torrentvpn (/usr/bin/date --iso-8601=seconds)" torrent killed (no VPN)"
     end
 end
-
